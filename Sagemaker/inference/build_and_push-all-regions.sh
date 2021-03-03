84e7621da06f8afaa09961c7197136b5012a2ec0
jackie <jackieliu_cornell@163.com>
2021-03-03 17:06:42 +0800

    update code

--- Sagemaker/inference/build_and_push-all-regions.sh
+++ Sagemaker/inference/build_and_push-all-regions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -v
-set -e
+# set -x
 # This script shows how to build the Docker image and push it to ECR to be ready for use
 # by SageMaker.
 
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
 image=$1
+profile=$2
 
 if [ "$image" == "" ]
 then
@@ -14,8 +14,14 @@ then
     image="paddle_crnn"
 fi
 
+if [ "$profile" == "" ]
+then
+    echo "Use profile=default"
+    profile="default"
+fi
+
 # Get the account number associated with the current IAM credentials
-account=$(aws sts get-caller-identity --query Account --output text)
+account=$(aws --profile ${profile} sts get-caller-identity --query Account --output text)
 
 if [ $? -ne 0 ]
 then
@@ -23,8 +29,9 @@ then
 fi
 
 # Get the region defined in the current configuration
-#if you only want to specify one region, use region, otherwise, use regions
-region=$(aws configure get region)
+regions=$(aws ec2 describe-regions --all-regions --query "Regions[].{Name:RegionName}" --output text)
+
+for region in $regions; do
 
 if [[ $region =~ ^cn.* ]]
 then
@@ -32,19 +39,27 @@ then
 else
     fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
 fi
+
 echo ${fullname}
 
-# Get the login command from ECR and execute it directly
-$(aws ecr get-login --registry-ids ${account} --region ${region} --no-include-email)
-
 # If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${image}" --region ${region} || aws ecr create-repository --repository-name "${image}" --region ${region}
+aws --profile ${profile} ecr describe-repositories --repository-names "${image}" --region ${region}
 
-aws ecr set-repository-policy \
+if [ $? -ne 0 ]
+then
+    aws --profile ${profile} ecr create-repository --repository-name "${image}" --region ${region} > /dev/null
+fi
+
+aws --profile ${profile} ecr set-repository-policy \
     --repository-name "${image}" \
     --policy-text "file://ecr-policy.json"
+
+# Get the login command from ECR and execute it directly
+$(aws --profile ${profile} ecr get-login --registry-ids ${account} --region ${region} --no-include-email)
 
 # Build the docker image, tag with full name and then push it to ECR
 docker build -t ${image} -f Dockerfile .
 docker tag ${image} ${fullname}
 docker push ${fullname}
+
+done
